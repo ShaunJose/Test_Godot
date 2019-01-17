56cbb5b96874e8c03ce563bbd2381cda2eca2c51
ShaunJose <shaun.jose12@gmail.com>
2019-01-17 16:33:45 +0000

    Centralisation of volume slider. neighbours hardcoded in settings. Autofocus added in all screens to allow mouse usage :D

--- Difficulty.gd
+++ Difficulty.gd
@@ -7,4 +7,5 @@ func _ready():
 	$Easy.set_button_group(group)
 	$Normal.set_button_group(group)
 	$BigBoi.set_button_group(group)
+	$Easy.grab_focus() # just so user can use arrowkeys straightaway
 	pass
--- NewGameScreen/NewGameScreen.gd
+++ NewGameScreen/NewGameScreen.gd
@@ -1,5 +1,7 @@
 extends Control
 
+func _ready():
+	$VBoxContainer/BackButton.grab_focus()
 
 func _on_BackButton_pressed():
 	get_tree().change_scene('res://MainScreen/MainScreen.tscn')
